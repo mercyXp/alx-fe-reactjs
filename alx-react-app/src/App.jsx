b2ebc80de02bc82cf78231b6e7a545cd14f1b08d
MercyXp <munzenzimercy2024@gmail.com>
2025-09-11 15:41:08 +0200

    Create Specific Components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import './App.css'
 
 function App() {
   return (
-    <WelcomeMessage />
+    <>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <Footer />
+    </>
   )
 }
 
