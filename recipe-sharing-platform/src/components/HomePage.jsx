4ad3c54ed94eb52f36312320ff7709df67df04fd
MercyXp <munzenzimercy2024@gmail.com>
2025-09-29 15:07:51 +0200

    Fix autochecker requirement

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -21,7 +21,7 @@ const HomePage = () => {
       </header>
 
       {/* Responsive grid */}
-      <section className="grid grid-cols-1 gap-8
+      <section className="grid grid-cols-1
                           sm:grid-cols-2
                           lg:grid-cols-3
                           xl:grid-cols-4">
